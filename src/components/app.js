ca409fa99e8da87c72992513b280ceb647ad3971
Sam Cook <sam.cook@suusuccess.org>
2020-11-17 14:41:40 -0700

    capitalization update

--- src/components/app.js
+++ src/components/app.js
@@ -9,7 +9,7 @@ import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
-import Blog from "./pages/blog";
+import Blog from "./pages/Blog";
 import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
